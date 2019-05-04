11c011cfd31d1e99e262ef007ad08c979bf839a0
Vincent Guyader <vincent@thinkr.fr>
2019-05-04 09:28:54 +0200

    add digits

---
+++ data-raw/genere_digit.R
@@ -0,0 +1,45 @@
+# DS-Digital
+library(magick)
+library(tidyverse)
+base <- image_read("blanc.png")
+image_annotate(base, "8", font = 'DS-Digital', size = 1300)
+image_annotate(base, "4", font = 'DS-Digital', size = 1300)
+image_annotate(base, "1", font = 'DS-Digital', size = 1300)
+image_annotate(base, "01234567890", font = 'DS-Digital', size = 1300)
+
+image_annotate(base, "7", font = 'DS-Digital', size = 1300) %>% 
+  image_fill("blue") %>%
+  image_crop( "540x880+50+210")
+image_annotate(base, "  1", font = 'DS-Digital', size = 1300) %>% 
+  image_fill("blue") %>%
+  image_crop( "540x880+330+230")
+
+
+
+
+image_resize()
+0:9 %>% map(
+~image_annotate(base, as.character(.x), font = 'DS-Digital', size = 1300) %>% 
+  # image_fill("blue") %>%
+  image_crop( "540x880+50+210") %>% 
+  image_write(glue::glue("{.x}.png"))
+)
+
+
+image_annotate(base, "", font = 'DS-Digital', size = 1300) %>% 
+  # image_fill("blue") %>%
+  image_crop( "540x880+50+210") %>% 
+  image_annotate( "1", font = 'DS-Digital', size = 1300,location = '+303-210') %>% 
+  image_write("1.png")
+image_annotate(base, "", font = 'DS-Digital', size = 1300) %>% 
+  # image_fill("blue") %>%
+  image_crop( "540x880+50+210") %>% 
+  image_annotate( ":", font = 'DS-Digital', size = 1300,location = '+303-210') %>% 
+  image_write("dot.png")
+
+glue::glue("{c(0:9,'dot')}.png")
+
+
+
+
+
